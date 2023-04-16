1078b0b7e408dca6686f0e09ba3757480891079f
Marcelo Barboza <salve.barboza@pm.me>
2023-04-16 11:57:55 -0300

    Store the converted number in a reusable format
    bconv actually converts from decimals to other numerical bases (see the
    comment to the bconv function for more details). Before, there was no
    way for us to automate checks to whether the results were correct or
    not. No, we can do that. I'll propose this as an exercise.

--- src/bconv.c
+++ src/bconv.c
@@ -5,58 +5,63 @@
 #define PRECISION 16
 #define EPSILON pow(0.1, PRECISION)
 
-void bconv(const int b, double f);
+typedef struct {
+  int *integer_part;
+  int integer_part_size;
+  int fractional_part[PRECISION];
+} real_number_t;
 
-int main(void) {
-  bconv(2, 0.9);
-  bconv(3, 0.9);
-  bconv(4, 0.9);
-  bconv(5, 0.9);
-  bconv(6, 0.9);
-  bconv(7, 0.9);
-  bconv(8, 0.9);
-  bconv(9, 0.9);
+void bconv(const int b, const double f, real_number_t *x);
+void print(const real_number_t x);
+
+int main(void)
+{
+  real_number_t x[7] = {{NULL, 0, {0}}};
+  bconv(2, 0.6, &x[0]);
+  bconv(2, 0.7, &x[1]);
+  print(x[0]);
+  print(x[1]);
+  free(x[0].integer_part);
+  free(x[1].integer_part);
   exit(EXIT_SUCCESS);
 }
 
-void bconv(const int b, double f) {
-  double int_f;
-  double fra_f;
-  int *int_f_in_nbase = NULL;
-  int fra_f_in_nbase[PRECISION] = {0};
-  double aprox_fra_f;
-  int n, i, e;
-
+/* 
+ * I think there's a bug in bconv where it does not correctly convert numbers
+ * to bases with more than 10 symbols. I'll fix it in future versions.
+ */
+void bconv(const int b, const double f, real_number_t *x)
+{
+  double int_f, fra_f, aprox_fra_f;
+  int n, i;
   fra_f = modf(f, &int_f);
-
   n = (int)int_f;
-
-  printf("%f (base 10) is approximately equal to ", f);
-
   if (n == 0) {
-    printf("0");
+    x->integer_part = 0;
   } else {
-    for (i = 1, e = 0; i <= n; i *= b, e++)
+    for (i = 1, x->integer_part_size = 0; i <= n; i *= b, (x->integer_part_size)++)
       ;
-    int_f_in_nbase = malloc(e * sizeof(int));
-    for (i = e - 1; i >= 0; i--, n /= b)
-      int_f_in_nbase[i] = n % b;
-    for (i = 0; i < e; i++)
-      printf("%d", int_f_in_nbase[i]);
-    free(int_f_in_nbase);
+    x->integer_part = malloc((x->integer_part_size) * sizeof(int));
+    for (i = (x->integer_part_size) - 1; i >= 0; i--, n /= b) {
+      x->integer_part[i] = n % b;
+    }
   }
+  for (i = 1, aprox_fra_f = 0.0; (i < PRECISION) && (fabs(aprox_fra_f - fra_f) >= EPSILON); i++) {
+    x->fractional_part[i] = (int)floor(pow(b, i) * fabs(fra_f - aprox_fra_f));
+    aprox_fra_f += x->fractional_part[i] * pow(b, -i);
+  }
+}
 
+void print(const real_number_t x)
+{
+  int i;
+  printf("\n");
+  for (i = 0; i < (x.integer_part_size); i++) {
+    printf("%d", x.integer_part[i]);
+  }
   printf(".");
-
-  for (i = 1, aprox_fra_f = 0.0;
-       (i < PRECISION) && (fabs(aprox_fra_f - fra_f) >= EPSILON); i++) {
-    fra_f_in_nbase[i] = (int)floor(pow(b, i) * fabs(fra_f - aprox_fra_f));
-    aprox_fra_f += fra_f_in_nbase[i] * pow(b, -i);
+  for (i = 0; i < PRECISION; i++) {
+    printf("%d", x.fractional_part[i]);
   }
-
-  for (i = 1; i < PRECISION; i++) {
-    printf("%d", fra_f_in_nbase[i]);
-  }
-
-  printf(" (base %d)\n", b);
+  printf("\n");
 }
